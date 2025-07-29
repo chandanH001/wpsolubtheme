653622c184848201a4fd3e443c638fe046c7f7d1
Chandan <chandan.halder96@gmail.com>
2025-07-30 01:44:17 +0600

    meta img not showing fix done

--- single.php
+++ single.php
@@ -6,8 +6,8 @@
             <div class="col-lg-8">
                 <div class="tp-postbox-wrapper">
                     <?php if (have_posts()): while (have_posts()): the_post(); ?>
-<?php get_template_part('/template-parts/content', get_post_format())?>
-<?php endwhile;else: ?>
+                    <?php get_template_part('/template-parts/content', get_post_format())?>
+                    <?php endwhile;else: ?>
                     <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
                     <?php endif; ?>
 
--- template-parts/header/header-1.php
+++ template-parts/header/header-1.php
@@ -45,7 +45,7 @@
                                 <path
                                     d="M8 3.8V8L10.8 9.4M15 8C15 11.866 11.866 15 8 15C4.13401 15 1 11.866 1 8C1 4.13401 4.13401 1 8 1C11.866 1 15 4.13401 15 8Z"
                                     stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>                                   <?php echo esc_html($opening_hours) ?></span>
+                            </svg> <?php echo esc_html($opening_hours) ?></span>
                     </div>
                     <?php endif?>
 
@@ -120,7 +120,7 @@
                             </a>
                         </div>
                         <?php endif?>
-<?php endif?>
+                        <?php endif?>
 
                         <div class="tp-header-2-hamburger d-xl-none offcanvas-open-btn">
                             <button class="hamburger-btn">
