edc77740a195fa3d558ce478edb3f52cab525a41
Chandan <chandan.halder96@gmail.com>
2025-07-31 23:03:25 +0600

    archive template and search template add

--- archive.php
+++ archive.php
@@ -2,7 +2,7 @@
 
 <section class="tp-postbox-ptb p-relative pt-130 pb-120">
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
             <div class="col-lg-8">
                 <div class="tp-postbox-wrapper">
                     <?php if (have_posts()): while (have_posts()): the_post(); ?>
@@ -10,18 +10,10 @@
                     <?php endwhile;else: ?>
                     <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
                     <?php endif; ?>
-                    <?php get_template_part('/template-parts/biography', get_post_format())?>
+                    <div class="tp-pagination pt-30">
+                        <?php solub_pagination(); ?>
+                    </div>
 
-                    <?php if ( comments_open() || get_comments_number() ) :
-                            comments_template();
-                    endif; ?>
-
-
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="tp-sidebar-wrapper pl-45">
-                    <?php get_sidebar(); ?>
                 </div>
             </div>
         </div>
